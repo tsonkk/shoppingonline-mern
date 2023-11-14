c3e4c48de3b35992b3b063b4cf6189931430bfe1
SonKK <tranghongson@gmail.com>
2023-11-14 07:59:01 +0700

    init

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'clustersgp.8zgd6op.mongodb.net',
+  DB_USER: 'sonkk',
+  DB_PASS: 'pwd',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'sonkk@123',
+  JWT_EXPIRES: '31556952000', // in milliseconds (01 year = 31556952000 ms)
+  EMAIL_USER: 'xxx@gmail.com', // Gmail service
+  EMAIL_PASS: 'byju cvrp lnxz lpxo',
+};
+module.exports = MyConstants;
